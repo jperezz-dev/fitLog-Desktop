43c0887e55860b61f2055d0be5d7c827527b2d53
SpookyLuna <spookyluna666@gmail.com>
2026-02-17 22:02:28 +0100

    Vista de login, clases globales, boton
    - Creación de la vista de login
    - Creación de clases globales necesarias para varias vistas
    - Creación de botón como componente

---
+++ src/renderer/src/components/boton.vue
@@ -0,0 +1,24 @@
+<script setup>
+defineProps(['texto'])
+</script>
+
+<template>
+  <button class="botonEstilo">
+    {{ texto }}
+  </button>
+</template>
+
+<style scoped>
+.botonEstilo {
+  background-color: #ff5833c2;
+  box-shadow: 0px 5px 4px 0px rgba(0, 0, 0, 0.5);
+  color: white;
+  font-size: 5em;
+  text-align: center;
+  border-radius: 1.1em;
+  cursor: pointer;
+  width: 85%;
+  border: none;
+  padding: 20px;
+}
+</style>
--- src/renderer/src/views/Login.vue
+++ src/renderer/src/views/Login.vue
@@ -1,14 +1,65 @@
+<script setup>
+import boton from '../components/boton.vue'
+</script>
+
 <template>
   <div id="contenedor">
-    <div id="publicidadContenedor">
-      <h1>Descarga <a style="color: #FF5733">FitLog App</a></h1>
+    <div class="publicidadContenedor">
+      <h1 style="width: 40%">Descarga <a style="color: #ff5733">FitLog App</a></h1>
+      <div class="contenedorImagenDescripcion">
+        <span class="mockupApp"></span>
+        <div class="contenedorDescripcion">
+          <ul>
+            <li>
+              <a style="color: #ff5733">Notificaciones en tiempo real:</a> Recibe avisos si se
+              libera una plaza en tu clase favorita.
+            </li>
+            <li>
+              <a style="color: #ff5733">Gestión de reservas:</a> Reserva, cancela o modifica tus
+              clases desde cualquier lugar.
+            </li>
+            <li>
+              <a style="color: #ff5733">Historial a mano:</a> Consulta tus entrenamientos pasados y
+              próximos retos al instante.
+            </li>
+          </ul>
+          <div class="contenedorDescargas">
+            <span class="descargaPlay"></span>
+            <span class="descargaApple"></span>
+          </div>
+        </div>
+      </div>
     </div>
-    <div id="loginContenedor"></div>
+    <div class="contenedorLoginRegistro">
+      <div class="contenedorLogo">
+        <span class="logo"></span>
+      </div>
+      <div class="contenedorOpciones">
+        <a class="opcionSeleccionada">Inicio de sesión</a>
+        <a>Registro</a>
+      </div>
+      <div class="contenedorCampos">
+        <div class="input-group">
+          <span class="material-icons iconoIzquierda">mail_outline</span>
+          <input class="input" type="email" placeholder="Email" />
+        </div>
+        <div class="input-group">
+          <span class="material-icons iconoIzquierda">lock_outline</span>
+          <input class="input" type="password" placeholder="Contraseña" />
+        </div>
+        <a id="passOlvidada" style="margin-bottom: 5%">¿Has olvidado tu contraseña?</a>
+        <boton texto="Iniciar sesión"></boton>
+        <a style="font-size: 3.5em; color: white"
+          >¿No tienes cuenta?<a style="color: #ff5733; margin-left: 0.5em; cursor: pointer"
+            >Regístrate</a
+          ></a
+        >
+      </div>
+    </div>
   </div>
 </template>
 
 <style scoped>
-
 #contenedor {
   display: flex;
   flex-direction: row;
@@ -17,11 +68,10 @@
   align-items: center;
 }
 
-#publicidadContenedor {
-  width: 55%;
-  height: 90%;
-  border: solid 1px white;
-
+#passOlvidada {
+  color: white;
+  font-size: 2.5em;
+  align-self: flex-end;
+  cursor: pointer;
 }
-
 </style>
